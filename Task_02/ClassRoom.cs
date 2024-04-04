6f32bea945ceb59945d2723da431dc9fab3cfb9b
Igor Korzhan <igor.human@gmail.com>
2024-04-04 23:40:19 +0300

    Початок ClassRoom

---
+++ Task_02/ClassRoom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_02
+{
+    internal class ClassRoom
+    {
+        private object pupil1;
+        private object pupil2;
+        private object pupil3;
+        private object pupil4;
+        
+        public ClassRoom(object pupil1, object pupil2, object pupil3, object pupil4)
+        {
+            this.pupil1 = pupil1;
+            this.pupil2 = pupil2;
+            this.pupil3 = pupil3;
+            this.pupil4 = pupil4;
+        }
+
+
+    }
+
+
+}
--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+            // Delay.
+            Console.ReadKey();
+        }
